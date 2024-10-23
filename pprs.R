ae4af5a982cee33d3c30d54fd7e38d0fb9068c03
phanson2k <phanson@broadinstitute.org>
2024-10-23 16:26:27 +0000

    genotype data extraction

--- pprs.R
+++ pprs.R
@@ -10,9 +10,12 @@ args <- list() # Parse command-line args
 pieces <- tstrsplit(split="--", paste(commandArgs(T), collapse=" "))[-1]
 for(piece in pieces) { parts <- unlist(tstrsplit(piece," ")); args[[parts[[1]]]] <- parts[-1] }
 
+# Resolve patterns to actual files (e.g. *.bgen -> chr1.bgen, chr2.bgen)
 args$geno_files  <- list.files(dirname(args$geno_files ), pattern=basename(args$geno_files ), full.names=T)
-args$sample_file <- list.files(dirname(args$sample_file), pattern=basename(args$sample_file), full.names=T)
 args$score_file  <- list.files(dirname(args$score_file), pattern=basename(args$score_file  ), full.names=T)
+if(!is.null(args$sample_file)) {
+args$sample_file <- list.files(dirname(args$sample_file), pattern=basename(args$sample_file), full.names=T)
+}
 if(is.null(args$scratch_dir)) { args$scratch_dir <- "pprs_scratch" }; dir.create(args$scratch_dir, showWarnings=F)
 
 # --- Input validation ---
@@ -30,15 +33,14 @@ if(all(grepl("bgen$",args$geno_files))) {
   if( length(args$sample_file)==0) stop("Provided --sample_file does not exist!")
   # TODO would be good to detect index files and warn if not found
   geno_files_type <-"bgen" 
-} else if(all(grepl("bcf(.gz)?$|vcf(.gz)?$", args$geno_files))) {
+} else if(all(grepl("bcf(.gz)?$|vcf(.gz)?$",args$geno_files))) {
   message(paste(c("Detected .vcf/.bcf files:",args$geno_files),collapse='\n'))
   # TODO would be good to detect index files and warn if not found
   geno_files_type <- "vcf/bcf"
-} else if(all(grepl("bed$|bim$|fam$"))) {
-  message(paste(c("Detected PLINK1 files:",args$geno_files),collapse='\n'))
+} else if(all(grepl("bed$|bim$|fam$",args$geno_files))) {
+  args$geno_files <- unique(sub(".bed$|.bim$|.fam$","",args$geno_files)) # PLINK only takes the file prefix
   geno_files_type <- "plink1"
-} else if(all(grepl("pgen$|pvar$|psam$|bim$|fam$"))) {
-  message(paste(c("Detected PLINK1 files:",args$geno_files),collapse='\n'))
+} else if(all(grepl("pgen$|pvar$|psam$|bim$|fam$",args$geno_files))) {
   geno_files_type <- "plink2"
 } else { stop(paste(c("Input geno_files format is unsupported, or is a mix of different formats. Below is the list of detected geno_files:",args$geno_files),collapse='\n')) }
 message("Detected ",geno_files_type," files:\n", paste(args$geno_files,collapse='\n'))
@@ -47,49 +49,59 @@ message("Detected ",geno_files_type," files:\n", paste(args$geno_files,collapse=
 args$score_file <- setnames(fread(args$score_file), old=1:6, new=c("chr","pos","ref","alt","id","ea"))
 anyDuplicated(args$score_file$id)
 ###TODO
+#### Is now the time to check if the id format is the same between the geno_files and the score_file? Or wait until later?
 
 ## LDlink inputs
 ###TODO: If neither token nor pop provided, warn that no proxies will be gotten if some variants are missing from geno_files. Complain if not both token & pop provided. If pop not provided, LDlinkR::list_pop(), but warn that ALL or multiple pops may be slower. 
 
 # --- Finished input validation ---
 
-
 # TODO: Proxies. See what variants are missing from the genotype files, grab proxies, check if any of _those_ are missing, repeat. Don't forget to update score file df with the proxies.
-
-# Extract genotype data for the variants in the --score_file.  
-if(geno_files_type=="bgen") {
-  ranges <- file.path(args$scratch_dir,"variant_ranges.txt")
-  ids    <- file.path(args$scratch_dir,"variant_ids.txt"   )
-  args$score_file[, writeLines(paste0(chr,":",pos,"-",pos), ranges) ]
-  args$score_file[, writeLines(id,                            ids ) ]
-  mclapply(geno_files, \(gf) system(paste0(
-    "bgenix -g ",gf,
-    " -incl-ranges ",ranges,
-    " -incl-rsids ",ids,
-    " > ",args$scratch_dir,"/",gf,"-subset"
-  )))
-} else if(geno_files_type="vcf/bcf") {
-} else if(geno_files_type="plink1") {
-} else if(geno_files_type="plink2") {
-} else stopifnot(F)
-
-system(paste("bgenix -list -g",files[[16]],"-incl-range","chr16:5378598-5378599"))
-system(paste("bgenix -list -g",files[[18]],"-incl-range","chr18:60217517-60217517"))
-
-# TODO: Take the list of variants found, compare against original score file to see if any missing
-#   separate fxns for bgenix and bcftools and plink extract, they should both output a df harmonzied to same format
-# Then if any missing put them all into LDproxy_batch
-#   TODO need to see what kind of format LDproxy_batch returns
-
+# Why not use PLINK to read all formats? Because it cannot make use of .bgi (bgen) or .csi (vcf/bcf) index files. Instead it would try to convert everything to plink2 format first, (very slow for huge datasets).
 ldprox_test <- LDproxy_batch(c("chr16:53785981","chr18:60217517"), token="1d5a0d43036d", genome_build="grch38")
-
 tmp <- fread("chr16:53785981_grch38.txt")
 
-# PLINK2
-bgenix \
-	-g /broad/ukbb/imputed_v3/ukb_imp_chr${chr}_v3.bgen \
-	-incl-rsids ${prs_dir}/${tag}_prs_snps.txt \
-	> ${prs_dir}/${tag}_prs_subset_chr${chr}.bgen
+# Extract full genotype data for the variants in the --score_file.  
+# Why not use PLINK to read all formats? Because it cannot make use of .bgi (bgen) or .csi (vcf/bcf) index files. Instead it would try to convert everything to plink2 format first, (very slow for huge datasets).
+ranges <- file.path(args$scratch_dir,"variant_ranges.txt")
+ids    <- file.path(args$scratch_dir,"variant_ids.txt"   )
+if(geno_files_type=="bgen") {
+  args$score_file[, writeLines(paste0(chr,":",pos,"-",pos), ranges) ]
+  args$score_file[, writeLines(id,                            ids ) ]
+  mclapply(args$geno_files, \(gf) system(paste0(
+    "bgenix -g ",gf,
+    " -incl-range ",ranges,
+    " -incl-rsids ",ids,
+    " > ",args$scratch_dir,"/",basename(gf),"-subset"
+  )))
+} else if(geno_files_type=="vcf/bcf") {
+  args$score_file[, writeLines(paste0(chr,'\t',pos), ranges) ]
+  args$score_file[, writeLines(id,                     ids ) ]
+  mclapply(args$geno_files, \(gf) system(paste0(
+    "bcftools view ",gf,
+    " -R ",ranges,
+    " -i'ID=@",ids,"'",
+    " > ",args$scratch_dir,"/",basename(df),"-subset"
+  )))
+} else if(geno_files_type=="plink1") {
+  args$score_file[, writeLines(paste0(chr,":",pos,"-",pos), ranges) ]
+  mclapply(args$geno_files, \(gf) system(paste0(
+    "plink2 --bfile",gf,
+    " --extract bed0 ",ranges,
+    " --make-pgen --out ", args$scratch_dir,"/",basename(gf),"-subset" # Might as well conver to pgen now
+  )))
+} else if(geno_files_type=="plink2") {
+  args$score_file[, writeLines(paste0(chr,":",pos,"-",pos), ranges) ]
+  mclapply(args$geno_files, \(gf) system(paste0(
+    "plink2 --pfile",gf,
+    " --extract bed0 ",ranges,
+    " --make-pgen --out ", args$scratch_dir,"/",basename(gf),"-subset"
+  )))
+} else { stopifnot(F) }
+
+
+
+
 
 system(paste("plink2",
 	"--bgen", f, # will be looping through all the files
