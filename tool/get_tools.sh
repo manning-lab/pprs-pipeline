33c4f6fbf95a33a65455c8790b31b937e250e3cb
phanson2k <phanson@broadinstitute.org>
2024-12-02 10:53:58 -0500

    Forgot to commit get_tools README

--- tool/get_tools.sh
+++ tool/get_tools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pwd | grep -q "tool$" || { echo "Please run this script from the tool/ folder."; exit 1; }
+pwd | grep -q "tool$" || { echo 'Please run this script from the tool/ folder.'; exit 1; }
 
 install_bgenix() {
-  echo "Getting bgenix... (this takes a long time to compile)"
-   if [ -d "bgen.tgz" ]; then
+  echo 'Getting bgenix... (this takes a long time to compile)'
+   if [ -d 'bgen.tgz' ]; then
      rm -r bgen.tgz # In case user got impatient and leaves a half-untarred directory
    fi
    curl -LO http://code.enkre.net/bgen/tarball/release/bgen.tgz
@@ -18,7 +18,7 @@ install_bgenix() {
 
 # Function to install bcftools
 install_bcftools() {
-  echo "Getting bcftools..."
+  echo 'Getting bcftools...'
   git clone --depth 1 --recurse-submodules https://github.com/samtools/htslib.git
   git clone --depth 1 https://github.com/samtools/bcftools.git
   cd bcftools
@@ -28,7 +28,7 @@ install_bcftools() {
 
 # Function to install plink
 install_plink() {
-  echo "Getting plink2..."
+  echo 'Getting plink2...'
   curl -LO https://s3.amazonaws.com/plink2-assets/alpha6/plink2_linux_avx2_20241111.zip
   unzip -o plink2_linux_avx2_20241111.zip plink2
 }
@@ -40,10 +40,10 @@ if [ $# -eq 0 ]; then
   install_plink
 else
   case "$1" in
-    "bgenix") install_bgenix ;;
+    "plink")    install_plink ;;
     "bcftools") install_bcftools ;;
-    "plink") install_plink ;;
-    *) echo "Invalid argument. Please use 'bgenix', 'bcftools', or 'plink'."
+    "bgenix")   install_bgenix ;;
+    *) echo 'Invalid argument. Please provide "bgenix", "bcftools", or "plink".'
        exit 1 ;;
   esac
 fi
