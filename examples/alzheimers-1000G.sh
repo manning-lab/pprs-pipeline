cd643b019a99cdd9a05bd61fbd051181e55bb6b2
phanson2k <phanson@broadinstitute.org>
2024-12-27 08:26:08 -0500

    Update examples

--- examples/alzheimers-1000G.sh
+++ examples/alzheimers-1000G.sh
@@ -1,20 +1,19 @@
-# 1000 Genomes files have no IDs, so we fill the IDs with chr:pos:ref:alt.
 Rscript ../pprs.R "
   --geno_files http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000_genomes_project/release/20181203_biallelic_SNV/*.vcf.gz
-  --fill_vcf_ids_with chr:pos:ref:alt
 
   --score_file ../score_files/alzheimers.csv
 
   --score_file_chr_col Chr
   --score_file_pos_col Pos
-  --score_file_id_col chr_cpra_id
   --score_file_ref_col Ref
   --score_file_alt_col Alt
   --score_file_ea_col Effect_Allele
-  --score_file_weight_cols 10 Total_GRS 12 13 14 15 11
+  --score_file_weight_cols Apolipoprotein_B_Pos CRP_Neg Diffusivity_inferior_longitudinal_fasciculus_Neg ICVF_Cingulum_CingulateR Proinsulin_Neg T2stat_PutamenL_Neg Total_GRS
 
   --output_fnm alzheimers-1000G.txt
 "
+
+# Other args you could add:
 # --threads 4 
 # --ldlink_token <YOUR TOKEN>
 # --ldlink_pop GBR
--- examples/alzheimers-1000G_high_coverage.sh
+++ examples/alzheimers-1000G_high_coverage.sh
@@ -1,20 +1,19 @@
-# 1000G files no IDs, so we fill the IDs with chr:pos:ref:alt.
 Rscript ../pprs.R "
   --geno_files http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20220422_3202_phased_SNV_INDEL_SV/*.vcf.gz$
-  --fill_vcf_ids_with chr:pos:ref:alt
 
   --score_file ../score_files/alzheimers.csv
 
   --score_file_chr_col Chr
   --score_file_pos_col Pos
-  --score_file_id_col chr_cpra_id
   --score_file_ref_col Ref
   --score_file_alt_col Alt
   --score_file_ea_col Effect_Allele
-  --score_file_weight_cols 10 Total_GRS 12 13 14 15 11
+  --score_file_weight_cols Apolipoprotein_B_Pos CRP_Neg Diffusivity_inferior_longitudinal_fasciculus_Neg ICVF_Cingulum_CingulateR Proinsulin_Neg T2stat_PutamenL_Neg Total_GRS
 
   --output_fnm alzheimers-1000G_high_coverage.txt
 "
+
+# Other args you could add:
 # --threads 4
 # --ldlink_token <YOUR TOKEN>
 # --ldlink_pop GBR
